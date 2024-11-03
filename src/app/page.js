e556e1c9270d346565d5a0078c92ac6c1f1aeb09
zylonox <shlok.agarwal0604@gmail.com>
2024-11-04 00:47:25 +0530

    aws upload done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import DemoSection from "@/components/DemoSection";
 import PageHeaders from "@/components/PageHeaders";
-import Upload from "@/components/Upload";
+import UploadForm from "@/components/UploadForm";
 
 export default function Home() {
   return (
@@ -9,12 +9,7 @@ export default function Home() {
         h1text="Add captions to your videos using Versify"
         h2text="Upload your videos we do the rest"
       />
-      <div className="flex justify-center">
-        <button className="bg-green-600 text-white px-4 py-2 rounded-xl flex gap-2">
-          <Upload />
-          Choose File
-        </button>
-      </div>
+      <UploadForm/>
       <DemoSection/>
     </>
   );
---
+++ src/components/UploadForm.js
@@ -0,0 +1,39 @@
+"use client";
+import Upload from "@/components/Upload";
+import axios from "axios";
+import { useState } from "react";
+const UploadForm = () => {
+  const [isUploading, setIsUploading] = useState(false);
+  async function upload(e) {
+    e.preventDefault();
+    const files = e.target.files;
+    if (files.length > 0) {
+      const file = files[0];
+      setIsUploading(true);
+      const res = await axios.postForm("/api/upload", { file });
+      setIsUploading(false);
+      console.log(res.data);
+    }
+  }
+  return (
+    <>
+      {isUploading ? (
+        <div className="bg-black/50 fixed inset-0 flex items-center justify-center">
+          <div className="flex flex-col items-center space-y-4">
+            <div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin"></div>
+            <p className="text-white">Uploading</p>
+          </div>
+        </div>
+      ) : null}
+      <div className="flex justify-center">
+        <label className="bg-green-600 text-white px-4 py-2 rounded-xl flex gap-2 cursor-pointer">
+          <Upload />
+          Choose File
+          <input onChange={upload} type="file" className="hidden" />
+        </label>
+      </div>
+    </>
+  );
+};
+
+export default UploadForm;
