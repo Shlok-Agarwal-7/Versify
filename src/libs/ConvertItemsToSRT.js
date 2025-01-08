ed9ce15ad159bbe41802b828fc3e3c716e641001
zylonox <shlok.agarwal0604@gmail.com>
2025-01-08 21:48:13 +0530

    added srt conversion

---
+++ src/libs/ConvertItemsToSRT.js
@@ -0,0 +1,18 @@
+function ConvertTimeToISO(time){
+    const d = new Date(parseFloat(time) * 1000);
+    return d.toISOString().substring(11, 23).replace('.', ',');
+}
+
+export default ConvertItemsToSRT => (items) => {
+    let srt = '';
+    let i = 1;
+    items.forEach((item) => {
+    const { start_time, end_time, content } = item;
+    srt += `${i}\n`;
+    srt += `${ConvertTimeToISO(start_time)} --> ${ConvertTimeToISO(end_time)}\n`;
+
+    srt += content + '\n';
+    srt += '\n';
+    i++;
+    });
+}
